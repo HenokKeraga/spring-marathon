abe84a54de44634d37f51546edfd22cb5318a18f
henok keraga <henokkeraga@gmail.com>
2022-04-23 23:02:31 -0600

    test'

---
+++ sm1/src/main/java/main/Main.java
@@ -0,0 +1,8 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+//        IoC -
+        var context= new AnnotationConfigApplicationContext();
+    }
+}
