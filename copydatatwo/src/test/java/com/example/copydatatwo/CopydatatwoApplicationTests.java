9a0854d6c9a455a1b484d741951384638a1dcd32
henok keraga <henokkeraga@gmail.com>
2022-08-03 21:34:14 -0600

    jdbc

---
+++ copydatatwo/src/test/java/com/example/copydatatwo/CopydatatwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.copydatatwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopydatatwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ copytable/src/test/java/com/example/copytable/CopytableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.copytable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopytableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
