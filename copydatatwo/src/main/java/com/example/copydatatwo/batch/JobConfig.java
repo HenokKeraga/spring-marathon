7951c7fca118df3bad374f266dad1cc1a7663dd6
henok keraga <henokkeraga@gmail.com>
2022-08-04 11:56:59 -0600

    jdbc

--- copydatatwo/src/main/java/com/example/copydatatwo/batch/JobConfig.java
+++ copydatatwo/src/main/java/com/example/copydatatwo/batch/JobConfig.java
@@ -5,19 +5,30 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.support.ListPreparedStatementSetter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.ArgumentPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterUtils;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Map.*;
 
 @Configuration
 public class JobConfig {
@@ -40,27 +51,29 @@ public class JobConfig {
 
 
     @Bean
-    public Job migrationJob() {
+    public Job migrationJob(Step migrationStep) {
 
         return jobBuilderFactory
                 .get("migrationJob")
                 .incrementer(new RunIdIncrementer())
-                .start(migrationStep())
+                .start(migrationStep)
                 .build();
     }
 
     @Bean
-    public Step migrationStep() {
+    public Step migrationStep(JdbcCursorItemReader<Department> jdbcCursorItemReader) {
         return stepBuilderFactory
                 .get("migrationStep")
                 .<Department, com.example.copydatatwo.model.sql.Department>chunk(1)
-                .reader(jdbcCursorItemReader())
+                .reader(jdbcCursorItemReader)
                 .writer(jdbcBatchItemWriter())
                 .build();
     }
 
     @Bean
-    public JdbcCursorItemReader<Department> jdbcCursorItemReader() {
+    @StepScope
+    public JdbcCursorItemReader<Department> jdbcCursorItemReader(@Value("#{jobParameters['id']}") Long id) {
+        System.out.println(" ----- " + id);
         var b = new BeanPropertyRowMapper<Department>();
         b.setMappedClass(Department.class);
 
@@ -70,21 +83,28 @@ public class JobConfig {
         }};
 
 
+
         return new JdbcCursorItemReaderBuilder<Department>()
+                .name("nm")
                 .dataSource(postgreDBDatasource)
-                .saveState(false)
-                .sql("Select id,dept_name as deptName from department")
+                //.sql("Select id,dept_name as deptName from department where id = ?")
+                .sql(NamedParameterUtils.substituteNamedParameters("Select id,dept_name as deptName from department where id = :id"
+                        , new MapSqlParameterSource(ofEntries(entry("id", id))))
+                )
+                .queryArguments(id)
 //                .rowMapper(new BeanPropertyRowMapper<>() {
 //                    {
 //                        setMappedClass(Department.class);
 //                    }
 //                })
 //                .rowMapper(b)
-                .rowMapper((resultSet, i) -> new Department() {{
-                            setId(resultSet.getLong("id"));
-                            setDeptName(resultSet.getString("deptName"));
-                        }}
-                )
+//                .rowMapper((resultSet, i) -> new Department() {{
+//                            setId(resultSet.getLong("id"));
+//                            setDeptName(resultSet.getString("deptName"));
+//                        }}
+//                )
+                .rowMapper(new BeanPropertyRowMapper<>(Department.class))
+                //  .preparedStatementSetter(new ArgumentPreparedStatementSetter(new Object[]{id}))
                 .build();
 
     }
