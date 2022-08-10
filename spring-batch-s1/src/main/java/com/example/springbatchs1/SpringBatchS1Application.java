bc0b2255074ef1178df35c015a98336fae71bad7
henok keraga <henokkeraga@gmail.com>
2022-08-09 22:01:32 -0600

    jdbcasfds

---
+++ spring-batch-s1/src/main/java/com/example/springbatchs1/SpringBatchS1Application.java
@@ -0,0 +1,13 @@
+package com.example.springbatchs1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchS1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchS1Application.class, args);
+    }
+
+}
---
+++ spring-batch-s1/src/test/java/com/example/springbatchs1/SpringBatchS1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchs1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchS1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
