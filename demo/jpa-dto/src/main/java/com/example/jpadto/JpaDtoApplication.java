84ff94df1f1590f6b402f07db7a9ebae5c117b27
henok keraga <henokkeraga@gmail.com>
2022-08-13 14:13:26 -0600

    dto

--- demo/jpa-dto/src/main/java/com/example/jpadto/JpaDtoApplication.java
+++ demo/jpa-dto/src/main/java/com/example/jpadto/JpaDtoApplication.java
@@ -1,15 +1,16 @@
 package com.example.jpadto;
 
 import com.example.jpadto.dto.ProductDTO;
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.*;
 import javax.transaction.Transactional;
@@ -70,4 +71,38 @@ class ProductController {
         return productRepository.findAllDTO();
     }
 
+    @GetMapping("/exe")
+    public List<ProductDTO> getAllProductExe(){
+        throw  new ApiRequestException("exception");
+    }
+
+}
+
+class ApiRequestException extends RuntimeException {
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+}
+@Builder
+@Data
+class ApiException{
+    String message;
+    String status;
+
+}
+@ControllerAdvice
+class ProductExceptionHandler{
+
+
+    @ExceptionHandler(ApiRequestException.class)
+    public ResponseEntity<Object> handleException(ApiRequestException e){
+  ApiException apiException= ApiException.builder()
+          .message(e.getMessage())
+          .status(HttpStatus.INTERNAL_SERVER_ERROR.toString())
+
+          .build();
+
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiException);
+    }
 }
